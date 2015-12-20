57fd9478bc7b2407b0c37edf911d5ee0bed01277
grant haywood <grant@iowntheinter.net>
2015-12-20 11:27:00 +0000

    worcx

---
+++ src/main/groovy/net/iowntheinter/cintershell/impl/SSHOperatorConsole.groovy
@@ -0,0 +1,82 @@
+package net.iowntheinter.cintershell.impl
+
+import io.vertx.core.DeploymentOptions
+import io.vertx.core.json.JsonObject
+import io.vertx.core.logging.LoggerFactory
+import io.vertx.core.net.JksOptions
+import io.vertx.ext.auth.shiro.ShiroAuthRealmType
+import io.vertx.ext.shell.ShellServiceOptions
+import io.vertx.ext.auth.shiro.ShiroAuthOptions
+import io.vertx.ext.shell.ShellServiceOptionsConverter
+import io.vertx.ext.shell.term.SSHTermOptions
+import io.vertx.groovy.core.Vertx
+import io.vertx.groovy.ext.shell.ShellService
+import io.vertx.ext.auth.shiro.LDAPProviderConstants
+
+/**
+ * Created by grant on 10/28/15.
+ */
+class SSHOperatorConsole {
+    def cmdset
+    def vertx
+    def logger = LoggerFactory.getLogger("SSHOperatorConsole")
+    def sshOpts = new SSHTermOptions()
+    def shadowmsg;
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
+    SSHOperatorConsole(vx, port) {
+        vertx = vx as Vertx
+        logger.info("attermpting to start ssh service on ${port}")
+        def JShiroOptions = new JsonObject()
+
+        JShiroOptions.put("type", ShiroAuthRealmType.LDAP).put("provider","shiro")
+                .put("config", new JsonObject()
+                .put(LDAPProviderConstants.LDAP_URL, "ldap://localhost:10389")
+                .put(LDAPProviderConstants.LDAP_USER_DN_TEMPLATE_FIELD, "uid={0},ou=system")
+                .put(LDAPProviderConstants.LDAP_AUTHENTICATION_MECHANISM, "simple")
+        )
+        JsonObject joptions = new JsonObject().put("sshOptions",
+                new JsonObject().
+                        put("host", "0.0.0.0").
+                        put("port", port).
+                        put("keyPairOptions", new JsonObject().
+                                put("path", "keystore.jks").
+                                put("password", "wibble")).put("authOptions",JShiroOptions));
+
+
+        try {
+            def bfile = "shadow-banner.txt"
+            def classloader = (URLClassLoader) (Thread.currentThread().getContextClassLoader())
+            def bpth = classloader.findResource(bfile);
+            logger.info("bfile ref: ${bpth}\n")
+
+            shadowmsg = classloader.getResourceAsStream(bfile).getText()
+            logger.info("${ANSI_RED + shadowmsg + ANSI_RESET}")
+        } catch (e) {
+            logger.error("could not get banner, ${e}")
+            shadowmsg = "Welcome to cintershell \n"
+        }
+        joptions.put('welcomeMessage',ANSI_RED + shadowmsg + ANSI_RESET)
+
+        def service = ShellService.create(vertx,joptions.getMap())
+
+        try {
+            service.start({  r ->
+                if (r.succeeded()) {
+                    logger.info("i think shell service is running:${r.result()}")
+                    logger.info(service)
+                } else {
+                    logger.error("ssh may not be running: ${r.cause()}")
+                }
+            })
+        } catch (e) {
+            logger.error("could not start ssh server: ${e}")
+
+        }
+
+    }
+
+
+}
+
